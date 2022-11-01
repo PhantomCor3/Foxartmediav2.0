847ccbfbc8e555cb75e08aa77bce3f5714a0b130
PhantomCor3 <oswaldo.andres.hernandez@gmail.com>
2022-11-01 00:46:27 -0600

    Update slider.php

--- php/secction/slider.php
+++ php/secction/slider.php
@@ -1,47 +1,153 @@
-<!-- Slider 1 Area Start Here -->               
-<div class="slider1-area">
-    <div class="bend niceties preview-1">
-        <div id="ensign-nivoslider-3" class="slides">   
-            <img src="img/slider/1.jpg" alt="slider" title="#slider-direction-1">
-            <img src="img/slider/2.jpg" alt="slider" title="#slider-direction-2">
-            <img src="img/slider/3.jpg" alt="slider" title="#slider-direction-3">
-        </div>
-        <div id="slider-direction-1" class="t-cn slider-direction">
-            <div class="slider-content s-tb slider-1">
-                <div class="title-container s-tb-c">
-                    <h2 class="title1"><?php echo $Phrase[0];?></h2>
-                    <h3><?php echo $Phrase[0];?></h3>
-                    <div class="slider-btn-area">
-                        <a href="about.php" class="btn-slider-fill">Descubre mas</a>
-                        <a href="contact.php" class="btn-slider-fill">Contactenos</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div id="slider-direction-2" class="t-cn slider-direction">
-            <div class="slider-content s-tb slider-2">
-                <div class="title-container s-tb-c">
-                    <h2 class="title1"><?php echo $Phrase[2];?></h2>
-                    <h3><?php echo $Experience;?></h3>
-                    <div class="slider-btn-area">
-                        <a href="about.php" class="btn-slider-fill">Descubre mas</a>
-                        <a href="contact.php" class="btn-slider-fill">Contactanos</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div id="slider-direction-3" class="t-cn slider-direction">
-            <div class="slider-content s-tb slider-3">
-                <div class="title-container s-tb-c">
-                    <h2 class="title1"><?php echo $Phrase[3];?></h2>
-                    <h3><?php echo $Phrase[1];?></h3>
-                    <div class="slider-btn-area">
-                        <a href="about.php" class="btn-slider-fill">Descubre mas</a>
-                        <a href="contact.php" class="btn-slider-fill">contactanos</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Slider 1 Area End Here -->
+.cursor
+    .cursor_point
+    .cursor_outer
+.portfolio
+    .portfolio_home
+        %div{:style => 'position: fixed; z-index: -99; width: 2320px; height: 180%; left: 0;top: -38%;'}
+            <iframe frameborder="0" height="100%" width="100%" src="https://youtube.com/embed/tz8Puc4W5BM?autoplay=1&controls=0&showinfo=0&autohide=1&mute=1"></iframe>
+        .portfolio_home__header
+            .hamburger.trigger
+                -(1..3).each do
+                    .hamburger_part
+        .portfolio_home__title
+            .logo
+                %img.first{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/clogotemp.png'}
+                %img.second{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/clogotemp2.png'}
+                .page_portfolio
+                    .portfolio_home__header
+                        .logoMain   
+                            %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/cagencylogo.png'}
+                        .nav
+                            %ul
+                                %li.active.trigger Our work
+                                %li.trigger Our services
+                                %li.trigger About us
+                                %li.trigger Contact us
+                        .number.black 0161 345 3464
+                        .hamburger.black.trigger
+                            -(1..3).each do
+                                .hamburger_part
+                    .slider_note
+                        Drag through our work
+                    .portfolio_home__work
+                        .portfolio_home__header.work
+                            .back
+                                
+                                %img.trigger{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/arrowDown.png'}
+                            .logoMain   
+                                %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/slogowhite.png'}
+                            .nav
+                                %ul
+                                    %li.active.trigger Our work
+                                    %li.trigger Our services
+                                    %li.trigger About us
+                                    %li.trigger Contact us
+                            .number.white 0161 345 3464
+                            .hamburger.white.trigger
+                                -(1..3).each do
+                                    .hamburger_part
+                        .slideClone
+                            .title.f
+                                #{'.01'} 
+                                %br
+                                My Protein
+                            .image
+                                %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/fud.png', :draggable => 'false'}
+                                .overlay
+                                .cats ADVERTISING   DESIGN   DIGITAL
+                                .title  My protein rebrand and digital campaign
+                            
+                        .slideClone
+                            .title.f
+                                #{'.02'} 
+                                %br
+                                Nike Air Max
+                            .image
+                                %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/nike.png', :draggable => 'false'}
+                                .overlay
+                                .cats ADVERTISING   DESIGN   DIGITAL   STRATEGY
+                                .title  Nike Air max video campaign 2017
+                        .slideClone
+                            .title.f
+                                #{'.03'} 
+                                %br
+                                Apple Watch
+                            .image
+                                %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/rpo.jpg', :draggable => 'false'}
+                                .overlay
+                                .cats ADVERTISING   DIGITAL   STRATEGY
+                                .title The new Apple Watch digital campaign 2019
+                        .slideClone
+                            .title.f
+                                #{'.04'} 
+                                %br
+                                Jade Teriyaki
+                            .image
+                                %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/orangetyhing.png', :draggable => 'false'}
+                                .overlay
+                                .cats ADVERTISING   DESIGN   DIGITAL   STRATEGY
+                                .title Another agency did this campaign, not us
+                        %img.scroll{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/arrowDown.png'}
+                    .portfolio_home__slider
+                        .slider_inner
+                            .slider_inner__slide
+                                .title 
+                                    #{'.01'} 
+                                    %br
+                                    My Protein
+                                .image
+                                    %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/fud.png', :draggable => 'false'}
+                                    .overlay
+                                    .cats ADVERTISING   DESIGN   DIGITAL
+                                    .title My protein rebrand and digital campaign
+                                    .button 
+                                        View case study
+                                        %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/arrowbblakc.png'}
+                            .slider_inner__slide
+                                .title 
+                                    #{'.02'} 
+                                    %br
+                                    Nike Air Max
+                                .image
+                                    %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/nike.png', :draggable => 'false'}
+                                    .overlay
+                                    .cats ADVERTISING   DESIGN   DIGITAL   STRATEGY
+                                    .title Nike Air max video campaign 2017
+                                    .button 
+                                        View case study
+                                        %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/arrowbblakc.png'}
+                            .slider_inner__slide
+                                .title 
+                                    #{'.03'} 
+                                    %br
+                                    Apple Watch
+                                .image
+                                    %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/rpo.jpg', :draggable => 'false'}
+                                    .overlay
+                                    .cats ADVERTISING   DIGITAL   STRATEGY
+                                    .title The new Apple Watch digital campaign 2019
+                                    .button 
+                                        View case study
+                                        %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/arrowbblakc.png'}
+                            .slider_inner__slide
+                                .title 
+                                    #{'.04'} 
+                                    %br
+                                    Jade Teriyaki
+                                .image
+                                    %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/orangetyhing.png', :draggable => 'false'}
+                                    .overlay
+                                    .cats ADVERTISING   DESIGN   DIGITAL   STRATEGY
+                                    .title Another agency did this campaign, not us
+                                    .button 
+                                        View case study
+                                        %img{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/arrowbblakc.png'}
+                
+            %hr
+            %h1 
+                We are an Independent 
+                %span Creative Advertising 
+                &amp; 
+                %span Branding Agency
+            %img.trigger{:src => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/arrowDown.png'}           
+
